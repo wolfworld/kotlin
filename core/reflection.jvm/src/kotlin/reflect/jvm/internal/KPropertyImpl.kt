f122e48925fc3f1ff65ae02094c74b3ea8d9d113
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-06-01 19:42:59 +0300

    Support reflection for local delegated properties
     #KT-15222 Fixed

--- core/reflection.jvm/src/kotlin/reflect/jvm/internal/KPropertyImpl.kt
+++ core/reflection.jvm/src/kotlin/reflect/jvm/internal/KPropertyImpl.kt
@@ -178,6 +178,10 @@ internal abstract class KPropertyImpl<out R> private constructor(
 
 
 private fun KPropertyImpl.Accessor<*, *>.computeCallerForAccessor(isGetter: Boolean): FunctionCaller<*> {
+    if (KDeclarationContainerImpl.LOCAL_PROPERTY_SIGNATURE.matches(property.signature)) {
+        return FunctionCaller.ThrowingCaller
+    }
+
     fun isInsideClassCompanionObject(): Boolean {
         val possibleCompanionObject = property.descriptor.containingDeclaration
         if (DescriptorUtils.isCompanionObject(possibleCompanionObject) && !DescriptorUtils.isInterface(possibleCompanionObject.containingDeclaration)) {
